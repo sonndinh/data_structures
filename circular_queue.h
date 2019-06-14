ffa93445d9599257b3cc8b579f7ab3a60ec105e7
Son Dinh <technology789@gmail.com>
2019-06-13 23:44:11 -0500

    changes

--- circular_queue.h
+++ circular_queue.h
@@ -63,35 +63,3 @@ private:
     int capacity;
     int counter;	
 };
-
-
-struct Message {
-	string data;
-};
-
-const unsigned CAPACITY = 10;
-int main() {
-	CircularQueue<struct Message> buffer(CAPACITY);
-	
-	for (int i = 0; i < CAPACITY+1; i++) {
-		string str = "Hello world " + to_string(i);
-		struct Message msg = {str};
-		try {
-			buffer.push(msg);
-		} catch (exception &ex) {
-			cout << ex.what() << endl;
-		}
-	}
-	
-	for (int i = 0; i < CAPACITY+1; i++) {
-		try {
-			const struct Message& msg = buffer.top();
-			cout << "Message: " << msg.data << endl;
-			buffer.pop();
-		} catch (exception &ex) {
-			cout << ex.what() << endl;
-		}
-	}
-	
-	return 0;
-}
