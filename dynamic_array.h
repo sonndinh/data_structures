6c7955e38aa33c4db9fb02e0047652abb75be91d
Son Dinh <technology789@gmail.com>
2019-06-22 16:38:43 -0500

    basic methods for vector

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,4 +1,5 @@
 #include <initializer_list>
+#include <iterator> // std::distance
 
 
 // Implementation of dynamic-sized array, similar to STL vector.
@@ -32,11 +33,31 @@ public:
 	}
 
 	// Construct from initializer list.
-	DynamicArray(std::initializer_list<T> il) {}
+	DynamicArray(std::initializer_list<T> il) {
+		size_ = il.size();
+		capacity_ = 2 * size_;
+		arr_ = new T[capacity_];
+
+		size_t i = 0;
+		for (std::initializer_list<T>::iterator it = il.begin(); it != il.end(); ++it) {
+			*(arr_ + i) = *it;
+			++i;
+		}
+	}
 
 	// Construct from input iterators.
 	template <InputIterator>
-	DynamicArray(InputIterator first, InputIterator last) {}
+	DynamicArray(InputIterator first, InputIterator last) {
+		size_ = std::distance(first, last);
+		capacity_ = 2 * size_;
+		arr_ = new T[capacity_];
+
+		size_t i = 0;
+		for (InputIterator it = first; it != last; ++it) {
+			*(arr_ + i) = *it;
+			++i;
+		}
+	}
 
 	// Destructor.
 	~DynamicArray() {
@@ -66,7 +87,17 @@ public:
 	}
 
 	// Initializing assign.
-	DynamicArray& operator= (std::initializer_list<T> il) {}
+	DynamicArray& operator= (std::initializer_list<T> il) {
+		size_ = il.size();
+		capacity_ = 2 * size_;
+		arr_ = new T[capacity_];
+
+		size_t i = 0;
+		for (std::initializer_list<T>::iterator it = il.begin(); it != il.end(); ++it) {
+			*(arr_ + i) = *it;
+			++i;
+		}		
+	}
 
 	// Return the number of entries.
 	size_t size() const {
@@ -80,6 +111,14 @@ public:
 
 	// Resize with a new size.
 	void resize(size_t k) {
+		if (k < size_) {
+			for (size_t i = k; i < size_; ++i) {
+				arr_[i].~T();
+			}
+			size_ = k;
+		} else if (k > capacity_) {
+			
+		}
 	}
 
 	// Return the capacity.
@@ -89,11 +128,38 @@ public:
 
 	// Insert a new entry to the back.
 	// Double the capacity if full.
-	void push_back();
+	void push_back(const T& t) {
+		arr_[size_++] = t;
+
+		// Double the capacity.
+		if (size_ == capacity_) {
+			double_capacity();
+		}
+	}
+
+	// Similar but with an rvalue.
+	void push_back(T&& t) {
+		arr_[size_++] = t;
+
+		if (size_ == capacity_) {
+			double_capacity();
+		}
+	}
 
 	// Remove the last entry.
-	// Shink by half if there are less than a quarter of capacity occupied.
-	void pop_back();
+	// Shrink by half if there are less than a quarter of capacity occupied.
+	void pop_back() {
+		if (size_ == 0)
+			return;
+
+		--size_;
+		arr_[size_].~T();
+
+		// Shrink the capacity by half.
+		if (size_ < capacity_/4) {
+			halve_capacity();
+		}
+	}
 
 	// Return the first entry.
 	T& front() const {
@@ -106,13 +172,38 @@ public:
 	}
 
 	// Access entry at index pos. No bound checking.
-	T& operator[] (size_t pos);
+	T& operator[] (size_t pos) {
+		return arr_[pos];
+	}
 
 	// Similar but return constant reference.
-	const T& operator[] (size_t pos) const;
+	const T& operator[] (size_t pos) const {
+		return arr_[pos];
+	}
+
+private:
+	void double_capacity() {
+		capacity_ *= 2;
+		copy_to_new_array();
+	}
+	
+	void halve_capacity() {
+		capacity_ /= 2;
+		copy_to_new_array();
+	}
+
+	void copy_to_new_array() {
+		T *temp = new T[capacity_];
+		for (size_t i = 0; i < size_; ++i) {
+			temp[i] = arr_[i];
+			// NOTE: should I expicitly call destructor for each entry in arr_.
+		}
+		delete[] arr_;
+		arr_ = temp;		
+	}
 	
 private:
-	static const size_t DEFAULT_CAP = 64;
+	static const size_t DEFAULT_CAP = 4;
 	T *arr_;
 	size_t size_;
 	size_t capacity_;
